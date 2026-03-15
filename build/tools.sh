6f35dc52a218fa1385aeec98d9b646616a913c09
David <dosa.dav@gmail.com>
2026-03-15 15:52:29 +0000

    Add all required build scripts

---
+++ build/tools.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+build_swu() {
+    KOBRA_MODEL_CODE=$1
+    UPDATE_DIRECTORY=${2:-/tmp/update_swu}
+    SWU_PATH=${3:-/build/dist/update.swu}
+
+    SWU_DIR=$(dirname $SWU_PATH)
+    SWU_NAME=$(basename $SWU_PATH)
+
+    mkdir -p $SWU_DIR/update_swu
+    rm -rf $SWU_DIR/update_swu/*
+
+    cd $UPDATE_DIRECTORY
+    tar -cf $SWU_DIR/update_swu/setup.tar --exclude='setup.tar' .
+    gzip $SWU_DIR/update_swu/setup.tar
+
+    rm -f $SWU_PATH
+    cd $SWU_DIR
+
+    if [ "$KOBRA_MODEL_CODE" = "K2P" ] || [ "$KOBRA_MODEL_CODE" = "K3" ]; then
+        zip -0 -P U2FsdGVkX19deTfqpXHZnB5GeyQ/dtlbHjkUnwgCi+w= -r $SWU_NAME update_swu
+    elif [ "$KOBRA_MODEL_CODE" = "KS1" ]; then
+        zip -0 -P U2FsdGVkX1+lG6cHmshPLI/LaQr9cZCjA8HZt6Y8qmbB7riY -r $SWU_NAME update_swu
+    elif [ "$KOBRA_MODEL_CODE" = "K3M" ]; then
+        zip -0 -P 4DKXtEGStWHpPgZm8Xna9qluzAI8VJzpOsEIgd8brTLiXs8fLSu3vRx8o7fMf4h6 -r $SWU_NAME update_swu
+    else
+        echo "Unknown Kobra model code: $KOBRA_MODEL_CODE"
+        exit 1
+    fi
+}
